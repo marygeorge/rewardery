6cb711a8b6e631e3dbd649afe07f3fcacd9ee9fe
Mary George <reahgeorge@yahoo.com>
2017-10-17 22:19:40 -0400

    circle fill

--- client/src/components/Circle/Circle.js
+++ client/src/components/Circle/Circle.js
@@ -9,7 +9,8 @@ export const  Circle=props=>
      data-percent={parseInt(props.totalPoint)>parseInt(props.rewardPoint)?
         "100%"
         :
-        parseInt((props.totalPoint*100)/props.rewardPoint)+"%"}>
+        //parseInt((props.totalPoint*100)/props.rewardPoint)+"%"}>
+        (parseInt(props.totalPoint/props.rewardPoint)*100)+"%"}>
 </div>
         <h3>{props.rewardName}: {props.rewardPoint} points</h3>
 </div>;
